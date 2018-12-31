137894146b0a1fbdc0046673f0a3c6c27189a48d
Justin Schellenberg <justin.schellenberg@boldcommerce.com>
2018-12-31 13:38:21 -0600

    1.1.0

--- webpack.common.js
+++ webpack.common.js
@@ -1,30 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  watch: true,
   entry: {
     // BMSPolyfills: './polyfills/polyfills.js', // IF YOU REQUIRE POLYFILLS, uncomment and gt file location for more information
     BMS_Preact_Hello_World: './builds/BMS_Preact_Hello_World.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-      },
-    ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx'],
