fb376308ae634bda83c277536f486dd62dd1fc43
Justin Schellenberg <justin.schellenberg@boldcommerce.com>
2019-02-18 20:32:36 -0600

    V2 Upgrade

--- builds/BMS_Preact_Hello_World.js
+++ builds/BMS_Preact_Hello_World.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../react/App';
 
+import './test.scss';
+
 ReactDOM.render(<App />, document.querySelector('#bab-app'));
