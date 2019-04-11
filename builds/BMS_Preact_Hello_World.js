fe626e4891639cf99161e8b0fcaa24235136ddc5
Justin Schellenberg <justin.schellenberg@boldcommerce.com>
2019-04-10 19:59:05 -0500

    Update Preact Build for IE QA

--- builds/BMS_Preact_Hello_World.js
+++ builds/BMS_Preact_Hello_World.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../react/App';
+import { h, render } from 'preact'
+
+import ThemeSettings from '../ThemeSettings/ThemeSettings';
+
+import App from '../preact/App';
 
 import './test.scss';
 
-ReactDOM.render(<App />, document.querySelector('#bab-app'));
+window.BOLD = window.BOLD || {};
+window.BOLD.BMS = window.BOLD.BMS || {};
+window.BOLD.BMS.BUILDS = window.BOLD.BMS.BUILDS || {};
+
+const initializeApp = (AppRoot, data) => {
+  Object.freeze(ThemeSettings.createInstance(data)); // Please Don't Modify this object, If you need to modify it make your own Clone!
+  render(<App />, AppRoot);
+}
+
+window.BOLD.BMS.BUILDS.preactApp = initializeApp;
+
